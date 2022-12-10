b8034545b7c23005359520a2be4af9487efb4054
mecaneer23 <mecaneer23@gmail.com>
2022-12-10 09:16:36 -0600

    use old mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bots/PostBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bots/PostBot.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Base.Robot;
 import org.firstinspires.ftc.teamcode.Components.Arm;
-import org.firstinspires.ftc.teamcode.Components.AutoMecanum;
 import org.firstinspires.ftc.teamcode.Components.Camera;
 import org.firstinspires.ftc.teamcode.Components.EncoderMecanum;
 import org.firstinspires.ftc.teamcode.Components.Grabber;
@@ -18,8 +17,7 @@ public class PostBot extends Robot {
     public Arm arm;
     public Grabber grabber;
     public Mecanum mecanum;
-//    public EncoderMecanum encoderMecanum;
-    public AutoMecanum encoderMecanum;
+    public EncoderMecanum encoderMecanum;
 
     @Override
     protected void mapHardware(HardwareMap hardwareMap, Telemetry telemetry, LinearOpMode opMode, boolean isTeleOp) {
@@ -46,7 +44,7 @@ public class PostBot extends Robot {
             addComponents(mecanum);
         } else {
             this.encoderMecanum =
-                    new AutoMecanum(
+                    new EncoderMecanum(
                             opMode,
                             "frontLeft",
                             "frontRight",
